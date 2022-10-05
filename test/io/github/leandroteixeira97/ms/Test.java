ebfe4d37f235ee2c9aed754d6408d2a63da145fe
leandroteixeira97 <leandro.teixeira.sjc@gmail.com>
2022-10-04 22:34:51 -0300

    initialized junit and the test folder

---
+++ test/io/github/leandroteixeira97/ms/Test.java
@@ -0,0 +1,22 @@
+package io.github.leandroteixeira97.ms;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Test {
+
+	@org.junit.jupiter.api.Test
+	void testIfEqualsTwo() {
+
+		int a = 1 + 1;
+
+		assertEquals(2, a);
+	}
+
+	@org.junit.jupiter.api.Test
+	void testIfEqualsThree() {
+		int x = 2 + 10 - 9;
+
+		assertEquals(3, x);
+	}
+
+}
